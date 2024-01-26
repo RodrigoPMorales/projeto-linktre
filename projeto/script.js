94e3de9d687fa7ec619e3f630e05d82f817913dd
Rodrigo <rodrigopmorales21@gamil.com>
2024-01-26 17:49:18 -0300

    first commit

---
+++ projeto/script.js
@@ -0,0 +1,34 @@
+function toggleMode(){
+   const html = document.documentElement
+
+   /* if(html.classList.contains('light'))    {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }*/
+
+    /*para simplificar o códico podemos usar esse: */
+    
+    html.classList.toggle('light')
+
+    
+    //mudar a foto quando mudar o mode light
+
+    const img = document.querySelector('#profile img')
+
+    if (html.classList.contains('light')){
+        img.setAttribute('src', './assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+    }
+
+    //mudar descrção da foto quando mudar de modo
+
+    const alt = document.querySelector('#profile img')
+
+    if (alt.classList.contains('light')) {
+        alt.setAttribute('alt', 'Foto de Rodrigo escondendo o rosto com o celular')
+    } else {
+        alt.setAttribute('alt', 'Foto de Rodrigo sorrindo na frente do espelho')
+    }
+}
